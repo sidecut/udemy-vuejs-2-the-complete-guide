c7101676a31515d9b96c991df66fe71dc8004a39
Jim Raden <james.raden@gmail.com>
2017-07-07 00:51:52 -0400

    Add his changes to Servers.vue

--- src/components/Server/Servers.vue
+++ src/components/Server/Servers.vue
@@ -11,6 +11,18 @@
 </template>
 
 <script>
+    export default {
+        data() {
+            return {
+                servers: [
+                    { id: 1, status: "Normal" },
+                    { id: 2, status: "Critical" },
+                    { id: 3, status: "Unknown" },
+                    { id: 4, status: "Normal" },
+                ]
+            };
+        }
+    }
 </script>
 
 <style>
