0a89434fe072fd58bbfc60152495bc6ae00c1833
Jim Raden <james.raden@gmail.com>
2017-07-09 06:39:22 -0400

    Start Assignment 8

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,33 @@
 <template>
     <div class="container">
-        <app-header></app-header>
-        <hr>
         <div class="row">
-            <servers :currentServer="currentServer"></servers>
-            <app-server-details :server="currentServer"></app-server-details>
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary">Load Blue Template</button>
+                <button class="btn btn-success">Load Green Template</button>
+                <button class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <app-blue></app-blue>
+                <app-green></app-green>
+                <app-red></app-red>
+            </div>
         </div>
-        <hr>
-        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
-    import Header from './components/Shared/Header.vue';
-    import Footer from './components/Shared/Footer.vue';
-    import Servers from './components/Server/Servers.vue';
-    import ServerDetails from './components/Server/ServerDetails.vue';
-    import { eventBus } from "./main";
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
 
     export default {
         components: {
-            appHeader: Header,
-            Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        },
-        created() {
-            var self = this;
-            eventBus.$on("selectServer", (server, evt) => {
-                if (self.currentServer == server && evt.ctrlKey) {
-                    // If we're already selected and they hit the control key, clear the selection
-                    self.currentServer = void 0;
-                } else {
-                    self.currentServer = server;
-                }
-                console.log("Selected new server", server);
-            });
-            eventBus.$on("deselectServer", () => {
-                console.log("Deselect");
-                self.currentServer = void 0;
-            });
-        },
-        data() {
-            return {
-                currentServer: void 0 //{id: 10}
-            }
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
         }
     }
 </script>
 
-<style lang="less">
-    .status-good {
-        background-color: lighten(desaturate(lightgreen, 25%), 20%);
-    }
-
-    .status-unknown {
-        background-color: lighten(lightgray, 10%);
-    }
-
-    .status-critical {
-        background-color: lighten(desaturate(pink, 25%), 7.5%);
-    }
+<style>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-export const eventBus = new Vue({
-    methods: {
-        selectServer(server, evt) {
-            this.$emit("selectServer", server, evt);
-            // console.log("selectServer", server);
-        },
-        deselectServer() {
-            this.$emit("deselectServer");
-        }
-    }
-});
-
 new Vue({
-    el: '#app',
-    render: h => h(App)
+  el: '#app',
+  render: h => h(App)
 })
