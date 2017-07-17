b0504a0ab7fa95508eb41796cde9ca417e1dc377
Jim Raden <james.raden@gmail.com>
2017-07-17 19:28:54 -0400

    Clean, proper removeEventLister in idiomatic ES6.

--- src/directives/highlight.js
+++ src/directives/highlight.js
@@ -1,34 +1,26 @@
+class privateHighlightFunctions {
+    static highlightIt() {
+        // debugger;
+        this.style.backgroundColor = '#fffae3';
+    }
+    static unHighlightIt() {
+        // debugger;
+        this.style.backgroundColor = '';
+    }
+}
+
 export default {
-    appSpecialProps: {
-        highlightIt() {
-            // debugger;
-            this.style.backgroundColor = '#fffae3';
-        },
-        unHighlightIt() {
-            // debugger;
-            this.style.backgroundColor = '';
-        }
-    },
     bind(el, binding, vNode) {
         // debugger;
         el.style.cursor = 'pointer';
-        let highlightIt = binding.def.appSpecialProps.highlightIt;
-        let unHighlightIt = binding.def.appSpecialProps.unHighlightIt;
-        el.addEventListener('mouseover', highlightIt);
-        el.addEventListener('mouseout', unHighlightIt);
-
-        // let elt = el;
-        // window.setTimeout(function() {
-        //     elt.removeEventListener('mouseover', highlightIt);
-        // }, 2000);
+        el.addEventListener('mouseover', privateHighlightFunctions.highlightIt);
+        el.addEventListener('mouseout', privateHighlightFunctions.unHighlightIt);
     },
     unbind(el, binding, vNode) {
-        console.log("unbind", el, binding, vNode);
+        // console.log("unbind", el, binding, vNode);
         // debugger;
-        // TODO: figure out how to remove the event listener
-        let highlightIt = binding.def.appSpecialProps.highlightIt;
-        let unHighlightIt = binding.def.appSpecialProps.unHighlightIt;
-        el.removeEventListener('mouseover', highlightIt);
-        el.removeEventListener('mouseout', unHighlightIt);
+        el.removeEventListener('mouseover', privateHighlightFunctions.highlightIt);
+        el.removeEventListener('mouseout', privateHighlightFunctions.unHighlightIt);
+        // console.log("unbound");
     }
 }
