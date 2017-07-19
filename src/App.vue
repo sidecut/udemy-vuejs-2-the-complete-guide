5cfe2274d2b4881ff731449469633314cc7cb48a
Jim Raden <james.raden@gmail.com>
2017-07-19 16:30:33 -0400

    Changed from click to mouseenter/mouseleave.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1 v-app-on:click="toggleShowHelp">Directives Exercise</h1>
+                <h1
+                        v-app-on:mouseenter="showHelpFunc"
+                        v-app-on:mouseleave="hideHelpFunc"
+                >Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
                 <div v-show="showHelp">
@@ -26,6 +29,12 @@
         methods: {
             toggleShowHelp() {
                 this.showHelp = !this.showHelp;
+            },
+            showHelpFunc() {
+                this.showHelp = true;
+            },
+            hideHelpFunc() {
+                this.showHelp = false;
             }
         },
         directives: {
